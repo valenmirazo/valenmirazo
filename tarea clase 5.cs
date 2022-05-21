ada2fa248f9456bdf52fa8ee73788696ab5c608b
valenmirazo <105955445+valenmirazo@users.noreply.github.com>
2022-05-20 22:17:15 -0300

    Add files via upload

---
+++ tarea clase 5.cs
@@ -0,0 +1,53 @@
+﻿Console.WriteLine("HOLA MUNDO");
+    
+Console.Clear();
+
+Console.WriteLine("ingrese su nombre: ");
+
+string nombre = Console.ReadLine();
+
+Console.WriteLine("hola " + nombre);
+
+Console.WriteLine("quieres continuar? S para si o N para no");
+
+string respuesta = Console.ReadLine();
+
+if (respuesta.ToUpper() =="N" )
+
+{
+    Console.WriteLine("programa finalizado correctamente");
+        }
+
+else {
+    Console.WriteLine("la opcion no es valida");
+        }
+
+while (respuesta.ToUpper() == "S")
+{
+    Console.Clear();
+
+    Console.WriteLine("ingrese su nombre: ");
+
+    string nombre1 = Console.ReadLine();
+
+    Console.WriteLine("hola " + nombre1);
+
+    Console.WriteLine("quieres continuar? S para si o N para no");
+
+    respuesta = Console.ReadLine();
+
+    // prueba n°1
+    if (respuesta.ToUpper() == "N")
+
+    {
+        Console.WriteLine("programa finalizado correctamente");
+    }
+
+    else
+    {
+        Console.WriteLine("la opcion no es valida");
+    }
+
+
+}
+
